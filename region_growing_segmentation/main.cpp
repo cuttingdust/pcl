9ae2b334cabc735382a14c0ec6cba810177c3cfc
cuttingDust_47 <18862963156@163.com>
2024-04-01 08:29:41 +0800

    pcl 区域生长分割 添加资源

--- region_growing_segmentation/main.cpp
+++ region_growing_segmentation/main.cpp
@@ -2,13 +2,22 @@
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/search/kdtree.h>
+#include <pcl/search/search.h>
 #include <pcl/console/print.h>
 #include <pcl/console/parse.h>
 #include <pcl/console/time.h>
+#include <pcl/segmentation/region_growing.h>
+
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/passthrough.h>
 
 
 #include <cstdio>
 #include <iostream>
+#include <memory>
+#include <thread>
 
 // 对于 Windows 系统
 #ifdef _WIN32
@@ -68,7 +77,7 @@ int main(int argc, char *argv[])
     pcl::console::parse_argument(argc, argv, "-st", SmoothnessThreshold);
     pcl::console::parse_argument(argc, argv, "-ct", CurvatureThreshold);                     /// 设置输入参数方式
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    auto cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
     if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) == -1)
     {
         std::cout << "cloud reading faild" << std::endl;
@@ -79,9 +88,71 @@ int main(int argc, char *argv[])
     PCL_INFO("\Loading pcd file takes(seconds): %d\n", diff[0]);
 
     /// Normal estimation step(1 parameter)
-    auto tree = std::make_shared<pcl::search::Search<pcl::PointXYZ>>(); /// //创建一个指向kd树搜索对象的共享指针
+    pcl::search::Search<pcl::PointXYZ>::Ptr tree = std::shared_ptr<pcl::search::Search<pcl::PointXYZ> > (new pcl::search::KdTree<pcl::PointXYZ>); /// 创建一个指向kd树搜索对象的共享指针
+    pcl::PointCloud<pcl::Normal>::Ptr  normals(new pcl::PointCloud <pcl::Normal>);
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimation;
+    normal_estimation.setSearchMethod(tree);
+    normal_estimation.setInputCloud(cloud);
+    normal_estimation.setKSearch(50);
+    normal_estimation.compute(*normals);
 
-    PrintMemoryInfo();
+    pcl::IndicesPtr indices(new std::vector<int>);
+    pcl::removeNaNFromPointCloud(*cloud, *indices);
+
+    pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
+    reg.setMinClusterSize(50);
+    reg.setMaxClusterSize(1000000);
+    reg.setSearchMethod(tree);
+    reg.setNumberOfNeighbours(30);
+    reg.setInputCloud(cloud);
+    reg.setIndices(indices);
+    reg.setInputNormals(normals);
+    reg.setSmoothnessThreshold(3.f / 180.f * M_PI);
+    reg.setCurvatureThreshold(1.0f);
+
+    std::vector<pcl::PointIndices> clusters;
+    reg.extract(clusters);
+
+    std::cout << "Numbef of clusters is equal to" << clusters.size() << std::endl;
+    std::cout << "First cluster has "<< clusters[0].indices.size() << "points." << std::endl;
+    std::cout << "These are the indices og the points of the initial" << std::endl
+    << "cloud that belong to the first cluster: "<< std::endl;
+
+    std::size_t counter = 0;
+    while (counter < clusters[0].indices.size()) {
+      std::cout << clusters[0].indices[counter] << ",";
+      counter++;
+      if (0 == counter % 10) {
+        std::cout << std::endl;
+      }
+
+    std::cout << std::endl;
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud();
+//    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("3D vIewer"));
+//
+//    viewer->setBackgroundColor(0, 0, 0);
+//    viewer->addPointCloud<pcl::PointXYZRGB>(colored_cloud, "sample cloud");
+//    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+//    viewer->addCoordinateSystem (1.0);
+//    viewer->initCameraParameters ();
+//
+//    PrintMemoryInfo();
+//    /// 主循环
+//    while (!viewer->wasStopped ()) {
+//        viewer->spinOnce (100);  // 100毫秒刷新一次
+//        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//    }
+
+	pcl::visualization::CloudViewer viewer ("���������ָ��");
+	viewer.showCloud(colored_cloud);
+	while (!viewer.wasStopped ())
+	{
+	}//���п��ӻ�
+
+  }
+
+
     return 0;
 }
 
